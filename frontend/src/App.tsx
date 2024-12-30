cfa1e8acc53fdda717349627ecb3dbd20098f0dc
ayushpatil2122 <ayushitkhede4@gmail.com>
2024-12-30 19:16:32 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import {Sender} from './components/Sender'
+import {Receiver} from './components/Receiver'
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/sender" element = {<Sender/>}/>
+          <Route path = "/receiver" element = {<Receiver/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
